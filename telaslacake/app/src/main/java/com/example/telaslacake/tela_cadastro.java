3b1e4da72dbdca73667150ec68e5c4f7ab87020f
Gabriel Abat <Abat.gabriel@gmail.com>
2021-07-03 17:38:55 -0300

    adicionando tela de cadastro e pagamento

---
+++ telaslacake/app/src/main/java/com/example/telaslacake/tela_cadastro.java
@@ -0,0 +1,13 @@
+package com.example.telaslacake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
---
+++ telaslacake/app/src/main/java/com/example/telaslacake/tela_pagamento.java
@@ -0,0 +1,17 @@
+package com.example.telaslacake;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class tela_pagamento extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_tela_pagamento);
+
+
+        }
+    }
+
