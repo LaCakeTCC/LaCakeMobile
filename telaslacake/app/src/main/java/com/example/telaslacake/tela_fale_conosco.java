7ed2ed15e7999e397bd1f300a243e653a5db6107
José Leonardo <joseleonardo64@outlook.com.br>
2021-07-13 20:14:43 -0300

    Adicionando Tela Fale Conosco

---
+++ telaslacake/app/src/main/java/com/example/telaslacake/tela_fale_conosco.java
@@ -0,0 +1,16 @@
+package com.example.telaslacake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_fale_conosco extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_tela_fale_conosco);
+
+
+        }
+    }
+
