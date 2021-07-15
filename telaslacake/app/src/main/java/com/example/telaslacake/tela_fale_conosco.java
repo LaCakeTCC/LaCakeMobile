4322b70b912b285d282fb5fbb508413f2884a0c4
José Leonardo <joseleonardo64@outlook.com.br>
2021-07-15 14:16:46 -0300

    adicionando tela mapa e atualizando classes e dependências para Androidx

--- telaslacake/app/src/main/java/com/example/telaslacake/tela_fale_conosco.java
+++ telaslacake/app/src/main/java/com/example/telaslacake/tela_fale_conosco.java
@@ -1,6 +1,6 @@
 package com.example.telaslacake;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class tela_fale_conosco extends AppCompatActivity {
--- telaslacake/app/src/main/java/com/example/telaslacake/tela_sobre.java
+++ telaslacake/app/src/main/java/com/example/telaslacake/tela_sobre.java
@@ -1,5 +1,5 @@
 package com.example.telaslacake;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
