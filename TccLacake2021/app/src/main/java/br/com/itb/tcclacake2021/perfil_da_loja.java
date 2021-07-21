c2f3bfe826d4da07cede8d48cb909ed6489018c0
Gabriel Abat <Abat.gabriel@gmail.com>
2021-07-21 14:53:59 -0300

    adcionando tela de perfil da loja, tela de loading e tela de favoritos novamente

---
+++ TccLacake2021/app/src/main/java/br/com/itb/tcclacake2021/perfil_da_loja.java
@@ -0,0 +1,12 @@
+package br.com.itb.tcclacake2021;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class perfil_da_loja  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_da_loja);
+    }
+}
---
+++ TccLacake2021/app/src/main/java/br/com/itb/tcclacake2021/tela_loading.java
@@ -0,0 +1,12 @@
+package br.com.itb.tcclacake2021;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_loading extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_loading);
+    }
+}
