82d8afa448af2a946af4ec32e5612893b9679cc7
José Leonardo <joseleonardo64@outlook.com.br>
2021-08-04 02:11:20 -0300

    navegação da tela método de pagamento

---
+++ TccLacake2021/app/src/main/java/br/com/itb/tcclacake2021/fragment_profile.java
@@ -0,0 +1,30 @@
+package br.com.itb.tcclacake2021;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class fragment_profile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+
+        //Método de pagamento - Começo
+        TextView buttonpagamento = (TextView) findViewById(R.id.paymethod);
+        buttonpagamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pagamento = new Intent(fragment_profile.this, tela_pagamento.class);
+                startActivity(pagamento);
+            }
+        });
+        //Método de pagamento - Fim
+    }
+}
